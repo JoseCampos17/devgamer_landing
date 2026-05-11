f4bd95b28b3e8bdf128149118385deb63bcd95a6
JoseCampos17 <66966452+JoseCampos17@users.noreply.github.com>
2026-05-11 16:29:09 -0500

    Add files via upload

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useLocation } from 'wouter';
 import { Lock, Mail, Loader2, ArrowLeft } from 'lucide-react';
-import { useAuth } from '@/contexts/AuthContext.tsx';
+import { useAuth } from '@/contexts/AuthContext';
+
 
 import axios from 'axios';
 
